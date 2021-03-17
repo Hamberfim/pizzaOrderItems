54906cd16d161f225b3bfd092cc56db931670d30
Hamberfim <adhamlingithub@gmail.com>
2021-03-17 14:58:22 -0500

    initial upload

---
+++ pizzaOrderApplication/pizzaOrder.Designer.cs
@@ -0,0 +1,259 @@
+﻿
+namespace pizzaOrderApplication
+{
+    partial class pizzaOrder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitlePizzaCrustStyle = new System.Windows.Forms.Label();
+            this.rdBtnThinCrust = new System.Windows.Forms.RadioButton();
+            this.rdBtnNYHandTossed = new System.Windows.Forms.RadioButton();
+            this.rdBtnChicagoDeepDish = new System.Windows.Forms.RadioButton();
+            this.lblTitleToppings = new System.Windows.Forms.Label();
+            this.chbCheese = new System.Windows.Forms.CheckBox();
+            this.chbPepperoni = new System.Windows.Forms.CheckBox();
+            this.chbSausage = new System.Windows.Forms.CheckBox();
+            this.chbMushrooms = new System.Windows.Forms.CheckBox();
+            this.chbOnions = new System.Windows.Forms.CheckBox();
+            this.chbGreenOlives = new System.Windows.Forms.CheckBox();
+            this.chbBlackOlives = new System.Windows.Forms.CheckBox();
+            this.lstbIngredientList = new System.Windows.Forms.ListBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnShowSelection = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitlePizzaCrustStyle
+            // 
+            this.lblTitlePizzaCrustStyle.AutoSize = true;
+            this.lblTitlePizzaCrustStyle.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitlePizzaCrustStyle.Location = new System.Drawing.Point(24, 15);
+            this.lblTitlePizzaCrustStyle.Name = "lblTitlePizzaCrustStyle";
+            this.lblTitlePizzaCrustStyle.Size = new System.Drawing.Size(161, 19);
+            this.lblTitlePizzaCrustStyle.TabIndex = 0;
+            this.lblTitlePizzaCrustStyle.Text = "Pizza Crust ($3.99): ";
+            // 
+            // rdBtnThinCrust
+            // 
+            this.rdBtnThinCrust.AutoSize = true;
+            this.rdBtnThinCrust.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdBtnThinCrust.Location = new System.Drawing.Point(39, 37);
+            this.rdBtnThinCrust.Name = "rdBtnThinCrust";
+            this.rdBtnThinCrust.Size = new System.Drawing.Size(54, 20);
+            this.rdBtnThinCrust.TabIndex = 1;
+            this.rdBtnThinCrust.TabStop = true;
+            this.rdBtnThinCrust.Text = "Thin";
+            this.rdBtnThinCrust.UseVisualStyleBackColor = true;
+            // 
+            // rdBtnNYHandTossed
+            // 
+            this.rdBtnNYHandTossed.AutoSize = true;
+            this.rdBtnNYHandTossed.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdBtnNYHandTossed.Location = new System.Drawing.Point(39, 68);
+            this.rdBtnNYHandTossed.Name = "rdBtnNYHandTossed";
+            this.rdBtnNYHandTossed.Size = new System.Drawing.Size(171, 20);
+            this.rdBtnNYHandTossed.TabIndex = 2;
+            this.rdBtnNYHandTossed.TabStop = true;
+            this.rdBtnNYHandTossed.Text = "New York Hand-Tossed";
+            this.rdBtnNYHandTossed.UseVisualStyleBackColor = true;
+            // 
+            // rdBtnChicagoDeepDish
+            // 
+            this.rdBtnChicagoDeepDish.AutoSize = true;
+            this.rdBtnChicagoDeepDish.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdBtnChicagoDeepDish.Location = new System.Drawing.Point(39, 99);
+            this.rdBtnChicagoDeepDish.Name = "rdBtnChicagoDeepDish";
+            this.rdBtnChicagoDeepDish.Size = new System.Drawing.Size(148, 20);
+            this.rdBtnChicagoDeepDish.TabIndex = 3;
+            this.rdBtnChicagoDeepDish.TabStop = true;
+            this.rdBtnChicagoDeepDish.Text = "Chicago Deep Dish";
+            this.rdBtnChicagoDeepDish.UseVisualStyleBackColor = true;
+            // 
+            // lblTitleToppings
+            // 
+            this.lblTitleToppings.AutoSize = true;
+            this.lblTitleToppings.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitleToppings.Location = new System.Drawing.Point(24, 137);
+            this.lblTitleToppings.Name = "lblTitleToppings";
+            this.lblTitleToppings.Size = new System.Drawing.Size(186, 19);
+            this.lblTitleToppings.TabIndex = 4;
+            this.lblTitleToppings.Text = "Toppings ($1.25 each): ";
+            // 
+            // chbCheese
+            // 
+            this.chbCheese.AutoSize = true;
+            this.chbCheese.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chbCheese.Location = new System.Drawing.Point(39, 170);
+            this.chbCheese.Name = "chbCheese";
+            this.chbCheese.Size = new System.Drawing.Size(113, 20);
+            this.chbCheese.TabIndex = 5;
+            this.chbCheese.Text = "Extra-Cheese";
+            this.chbCheese.UseVisualStyleBackColor = true;
+            // 
+            // chbPepperoni
+            // 
+            this.chbPepperoni.AutoSize = true;
+            this.chbPepperoni.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chbPepperoni.Location = new System.Drawing.Point(39, 200);
+            this.chbPepperoni.Name = "chbPepperoni";
+            this.chbPepperoni.Size = new System.Drawing.Size(92, 20);
+            this.chbPepperoni.TabIndex = 6;
+            this.chbPepperoni.Text = "Pepperoni";
+            this.chbPepperoni.UseVisualStyleBackColor = true;
+            // 
+            // chbSausage
+            // 
+            this.chbSausage.AutoSize = true;
+            this.chbSausage.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chbSausage.Location = new System.Drawing.Point(39, 230);
+            this.chbSausage.Name = "chbSausage";
+            this.chbSausage.Size = new System.Drawing.Size(83, 20);
+            this.chbSausage.TabIndex = 7;
+            this.chbSausage.Text = "Sausage";
+            this.chbSausage.UseVisualStyleBackColor = true;
+            // 
+            // chbMushrooms
+            // 
+            this.chbMushrooms.AutoSize = true;
+            this.chbMushrooms.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chbMushrooms.Location = new System.Drawing.Point(39, 260);
+            this.chbMushrooms.Name = "chbMushrooms";
+            this.chbMushrooms.Size = new System.Drawing.Size(100, 20);
+            this.chbMushrooms.TabIndex = 8;
+            this.chbMushrooms.Text = "Mushrooms";
+            this.chbMushrooms.UseVisualStyleBackColor = true;
+            // 
+            // chbOnions
+            // 
+            this.chbOnions.AutoSize = true;
+            this.chbOnions.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chbOnions.Location = new System.Drawing.Point(39, 290);
+            this.chbOnions.Name = "chbOnions";
+            this.chbOnions.Size = new System.Drawing.Size(72, 20);
+            this.chbOnions.TabIndex = 9;
+            this.chbOnions.Text = "Onions";
+            this.chbOnions.UseVisualStyleBackColor = true;
+            // 
+            // chbGreenOlives
+            // 
+            this.chbGreenOlives.AutoSize = true;
+            this.chbGreenOlives.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chbGreenOlives.Location = new System.Drawing.Point(39, 320);
+            this.chbGreenOlives.Name = "chbGreenOlives";
+            this.chbGreenOlives.Size = new System.Drawing.Size(110, 20);
+            this.chbGreenOlives.TabIndex = 10;
+            this.chbGreenOlives.Text = "Green Olives";
+            this.chbGreenOlives.UseVisualStyleBackColor = true;
+            // 
+            // chbBlackOlives
+            // 
+            this.chbBlackOlives.AutoSize = true;
+            this.chbBlackOlives.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chbBlackOlives.Location = new System.Drawing.Point(39, 350);
+            this.chbBlackOlives.Name = "chbBlackOlives";
+            this.chbBlackOlives.Size = new System.Drawing.Size(104, 20);
+            this.chbBlackOlives.TabIndex = 11;
+            this.chbBlackOlives.Text = "Black Olives";
+            this.chbBlackOlives.UseVisualStyleBackColor = true;
+            // 
+            // lstbIngredientList
+            // 
+            this.lstbIngredientList.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstbIngredientList.FormattingEnabled = true;
+            this.lstbIngredientList.ItemHeight = 15;
+            this.lstbIngredientList.Location = new System.Drawing.Point(164, 166);
+            this.lstbIngredientList.Name = "lstbIngredientList";
+            this.lstbIngredientList.Size = new System.Drawing.Size(240, 199);
+            this.lstbIngredientList.TabIndex = 12;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(28, 385);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(120, 23);
+            this.btnClear.TabIndex = 13;
+            this.btnClear.Text = "&Clear Form";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnShowSelection
+            // 
+            this.btnShowSelection.Location = new System.Drawing.Point(164, 385);
+            this.btnShowSelection.Name = "btnShowSelection";
+            this.btnShowSelection.Size = new System.Drawing.Size(240, 23);
+            this.btnShowSelection.TabIndex = 14;
+            this.btnShowSelection.Text = "Show Order Selection";
+            this.btnShowSelection.UseVisualStyleBackColor = true;
+            this.btnShowSelection.Click += new System.EventHandler(this.btnShowSelection_Click);
+            // 
+            // pizzaOrder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 437);
+            this.Controls.Add(this.btnShowSelection);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lstbIngredientList);
+            this.Controls.Add(this.chbBlackOlives);
+            this.Controls.Add(this.chbGreenOlives);
+            this.Controls.Add(this.chbOnions);
+            this.Controls.Add(this.chbMushrooms);
+            this.Controls.Add(this.chbSausage);
+            this.Controls.Add(this.chbPepperoni);
+            this.Controls.Add(this.chbCheese);
+            this.Controls.Add(this.lblTitleToppings);
+            this.Controls.Add(this.rdBtnChicagoDeepDish);
+            this.Controls.Add(this.rdBtnNYHandTossed);
+            this.Controls.Add(this.rdBtnThinCrust);
+            this.Controls.Add(this.lblTitlePizzaCrustStyle);
+            this.Name = "pizzaOrder";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Personal Pizza Order Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitlePizzaCrustStyle;
+        private System.Windows.Forms.RadioButton rdBtnThinCrust;
+        private System.Windows.Forms.RadioButton rdBtnNYHandTossed;
+        private System.Windows.Forms.RadioButton rdBtnChicagoDeepDish;
+        private System.Windows.Forms.Label lblTitleToppings;
+        private System.Windows.Forms.CheckBox chbCheese;
+        private System.Windows.Forms.CheckBox chbPepperoni;
+        private System.Windows.Forms.CheckBox chbSausage;
+        private System.Windows.Forms.CheckBox chbMushrooms;
+        private System.Windows.Forms.CheckBox chbOnions;
+        private System.Windows.Forms.CheckBox chbGreenOlives;
+        private System.Windows.Forms.CheckBox chbBlackOlives;
+        private System.Windows.Forms.ListBox lstbIngredientList;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnShowSelection;
+    }
+}
+
---
+++ pizzaOrderApplication/pizzaOrder.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pizzaOrderApplication
+{
+    public partial class pizzaOrder : Form
+    {
+        public pizzaOrder()
+        {
+            InitializeComponent();
+
+            // set default crust selection
+            rdBtnThinCrust.Checked = true;
+        }
+
+        private void btnShowSelection_Click(object sender, EventArgs e)
+        {
+            lstbIngredientList.Items.Clear();
+
+            // logic to add items to the listbox
+            double priceTotal = 0;
+            // crust type
+            if (rdBtnThinCrust.Checked)
+            {
+                priceTotal = priceTotal + 3.99;
+                lstbIngredientList.Items.Add(rdBtnThinCrust.Text);
+            }
+            if (rdBtnNYHandTossed.Checked)
+            {
+                priceTotal = priceTotal + 3.99;
+                lstbIngredientList.Items.Add(rdBtnNYHandTossed.Text);
+            }
+            if (rdBtnChicagoDeepDish.Checked)
+            {
+                priceTotal = priceTotal + 3.99;
+                lstbIngredientList.Items.Add(rdBtnChicagoDeepDish.Text);
+            }
+            // toppings
+            if (chbCheese.Checked)
+            {
+                priceTotal = priceTotal + 1.25;
+                lstbIngredientList.Items.Add(chbCheese.Text);
+            }
+            if (chbPepperoni.Checked)
+            {
+                priceTotal = priceTotal + 1.25;
+                lstbIngredientList.Items.Add(chbPepperoni.Text);
+            }
+            if (chbSausage.Checked)
+            {
+                priceTotal = priceTotal + 1.25;
+                lstbIngredientList.Items.Add(chbSausage.Text);
+            }
+            if (chbMushrooms.Checked)
+            {
+                priceTotal = priceTotal + 1.25;
+                lstbIngredientList.Items.Add(chbMushrooms.Text);
+            }
+            if (chbOnions.Checked)
+            {
+                priceTotal = priceTotal + 1.25;
+                lstbIngredientList.Items.Add(chbOnions.Text);
+            }
+            if (chbGreenOlives.Checked)
+            {
+                priceTotal = priceTotal + 1.25;
+                lstbIngredientList.Items.Add(chbGreenOlives.Text);
+            }
+            if (chbBlackOlives.Checked)
+            {
+                priceTotal = priceTotal + 1.25;
+                lstbIngredientList.Items.Add(chbBlackOlives.Text);
+            }
+
+            // add price total to listbox
+            string orderTotal = $"Order Total: {priceTotal:C}";
+            lstbIngredientList.Items.Add(orderTotal);
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lstbIngredientList.Items.Clear();
+
+            // set default crust selection
+            rdBtnThinCrust.Checked = true;
+            chbCheese.Checked = false;
+            chbPepperoni.Checked = false;
+            chbSausage.Checked = false;
+            chbMushrooms.Checked = false;
+            chbOnions.Checked = false;
+            chbGreenOlives.Checked = false;
+            chbBlackOlives.Checked = false;
+        }
+    }
+}
